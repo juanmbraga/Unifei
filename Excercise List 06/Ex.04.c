d51d85466e17820e9fd560b00f7953ce924a1f35
Juan M. Braga <64935978+juanmbraga@users.noreply.github.com>
2020-05-19 12:02:29 -0300

    List 6 Excercise 4 done

---
+++ Excercise List 06/Ex.04.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*Questão 4. Faça uma função que imprima na tela o seguinte cabeçalho:  
+Curso Ciência da Computacao   
+Disciplina – Fundamentos de Programação  
+Universidade Federal de Itajubá
+ 
+Não é necessário implementar a função principal.  Nos demais exercícios, use essa função para imprimir esse cabeçalho.
+Com o que sabemos, você vai precisar copiar o código dessa função para o código dos respectivos programas.  
+*/
+
+int marcaunifei (void);
+
+int main (void) {
+    printf("\nCurso Ciência da Computacao\nDisciplina - Fundamentos de Programação\nUniversidade Federal de Itajubá\n");
+}
