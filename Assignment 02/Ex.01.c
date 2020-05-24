e376375814413deb7dc116ae87e2eb6593ae22a4
Juan M. Braga <64935978+juanmbraga@users.noreply.github.com>
2020-05-24 18:34:54 -0300

    Assignment 02 progress

---
+++ Assignment 02/Ex.01.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+/* 
+Dado qualquer N, se você aplicar repetidas vezes a seguinte função:
+
+f(N)= N / 2 , se N e par.
+f(N)= 3 * N + 1, se N for impar.
+
+o valor eventualmente será igual a 1. 
+Agora faça um programa que diz o número de vezes que a função é chamada antes que o valor de N seja igual a 1.
+
+Obs.: 1 <= N <= 100000
+*///pode ser possivel fazer com do/while -1
+
+int collatz (int a);
+
+int main () {
+    int i=0, num;
+    printf("Insira um numero qualquer: ");
+    scanf("%d", &num);
+
+    if (num >= 1 && num <= 100000) {
+
+        for (i=0; num==1; i++){
+            num = collatz (num);
+        }
+        
+        printf ("O numero de operacoes necessarias e igual a %d.", i);
+        printf("%d", num);
+    }
+    else
+    printf("Numero nao permitido, deve estar entre 1 e 100000.");    
+}
+
+int collatz (int a) {
+    if (a % 2 == 0)
+        return a/2;
+    else
+        return (a*3)+1;
+}
---
+++ Assignment 02/Ex.02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* 
+Escrever uma função que tem como parâmetros de entrada três valores inteiros (a, b,c)
+e retorna a posição do maior e a posição do menor valor. 
+Exemplo: Se a = 7, b = 1 e c = 5, a função deve retornar 2 como a posição do menor e 1 como a posição do maior.
+Use a função no programa principal considerando três valores digitados pelo usuário.
+*/
+
+//ate onde eu saiba nao ha como uma funcao retornar mais de um valor. Sera que por "retornar" a professora diz imprimir?
+
+int order (int a, int b, int c);
+
+int order (a,b,c) {
+
+}
