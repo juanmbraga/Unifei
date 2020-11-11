ea54e31bad598653193eacb8641aec5a4f4061c1
Juan M. Braga <64935978+juanmbraga@users.noreply.github.com>
2020-11-11 19:59:23 -0300

    Finished first version of main.c and pilha.h

---
+++ 2. Algorithms and Data Structures/Exercise List 5/main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pilha.h"
+
+
+int main (void) {
+
+    Pilha* pilha[3];
+    int retira, recebe, controle, movimentos=0;
+
+    // Alocar as pilhas
+    for(int i=0; i<3; i++){
+
+        pilha[i] = criarPilha();
+
+        // em caso de erro, avise e encerre o programa
+        if(pilha[0] == NULL){
+            printf("\nErro na alocacao de memoria. \nO programa ira encerrar.");
+            pausa();
+            liberarPilhas(pilha);
+            return -1;
+        }
+    }
+
+    // inicializar a primeira pilha 
+    // (uma vez que a distancia das letras nao e a mesma, nao e possivel evitar a repetitividade)
+    empilhar(pilha[0], 'U');
+    empilhar(pilha[0], 'O');
+    empilhar(pilha[0], 'I');
+    empilhar(pilha[0], 'E');
+    empilhar(pilha[0], 'A');
+
+    do{
+        system("clear");
+        printf("\nPseudo-Torre-de-Hanoi com letras!");
+        printf("\nTransfira todos os itens para a pilha 3...");
+        printf("\n... Mas sem alterar a ordem alfabetica!");
+
+        imprimePilha(pilha);
+
+        printf("\n\nDe qual pilha gostaria de retirar um cubo? ");
+        do{
+            scanf("%d", retira);
+        } while(retira < 1 && retira > 3);
+
+        retira--;
+
+        printf("\nPara qual pilha gostaria de transferi-lo?");
+        do{
+            scanf("%d", recebe);
+        } while(recebe < 1 && recebe > 3);
+
+        recebe--;
+        
+        controle = transferir(pilha[retira], pilha[recebe]);
+
+        switch (controle) {
+            case 0:
+                printf("\nInsercao realizada com sucesso!");
+                movimentos++;
+            break;
+
+            case 1: 
+                printf("\nOs itens nao ficariam em ordem! Tente novamente.");
+            break;
+
+            case 2: 
+                printf("\nMesma pilha selecionada! Tente novamente.");
+            break;
+
+            case -1:
+                printf("Erro de memoria na alocacao! Encerrando o programa.");
+                pausa();
+                retira = 33;
+                continue;
+            break;            
+        }
+
+        pausa();
+
+    } while(tamanhoDaPilha(pilha[2]) != 5 || retira != 33);
+
+    if (retira != 33){
+        system("clear");
+        printf("\n\nParabens! Voce Ganhou o jogo em %d movimentos!", movimentos);
+        imprimePilhas(pilha);
+    }
+
+    printf("\n\nEncerrando o jogo.");
+    pausa();
+    liberarPilhas(pilha);
+
+    return 0;
+}
---
+++ 2. Algorithms and Data Structures/Exercise List 5/pilha.h
@@ -0,0 +1,38 @@
+typedef struct descritor* Pilha;               
+
+
+// Funcao para criar uma pilha
+Pilha* criarPilha();
+//retornos: NULL= erro, !NULL= sucesso
+
+// Funcao para liberar um vetor de tres pilhas
+int liberarPilhas(Pilha* pilha[]);
+// retornos: 0= sucesso, 1= nao existe
+
+// Retorna o numero de elementos na pilha
+int tamanhoDaPilha(Pilha* pilha);
+//retornos: -1= nao existe, outros: tamanho
+
+// Funcao para inserir na pilha (sem checagem, apenas para o inicio)
+int empilhar(Pilha* pilha, char dado);
+//retornos: 0= sucesso, 1= fora de ordem, -1= nao existe
+
+// Funcao para retirar o primeiro item da pilha
+char desempilhar(Pilha* pilha);
+//retornos: 0= vazia ou inexistente, !0= letra retirada (sucesso)
+
+// Consultar o primeiro item da pilha
+char consultarTopoPilha(Pilha* pilha);
+//retornos: a decidir
+
+// Confere a ordem e tranfere letra de uma pilha para outra
+int transferir(Pilha* entrega, Pilha* recebe);
+//retornos: 0= sucesso, 1= fora de ordem, 2= mesma pilha, -1= nao existe
+//(nao e preciso checar se estiver cheia, uma vez que so existem cinco letras)
+
+// Imprime os elementos de tres pilhas da maneira pedida pelo exercicio
+void imprimePilhas(Pilha* pilha);
+
+// simplificar interacao
+void pausa();
+
